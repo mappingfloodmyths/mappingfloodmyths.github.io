9a23bbcef67e749f67e21f54ab4bef1d20841add
Ashley Rojas <arojas1@gradcenter.cuny.edu>
2021-10-11 20:57:19 -0400

    wave added

---
+++ script.js
@@ -0,0 +1,26 @@
+// var height: 60;
+
+var myWave = $('#myID').wavify({
+    height: 60,
+    bones: 3,
+    amplitude: 40,
+    color: '#2158A8',
+    // opacity: '.5',
+    speed: .25
+  });
+
+// function btnclick() {
+//     document.getElementById("waveup").classList.toggle("show");
+// }
+
+// const svg = d3.select(".body")
+//     .append("svg")
+//     .attr("width", "100%")
+//     .attr("height", "100%")
+//     .attr("version", "1.1")
+//     .attr("xmlns", "http://www.w3.org/2000/svg")
+// svg.append("defs")
+// svg.append("path")
+//     .attr("id", "myID")
+//     .attr("d", "")
+
